cb82067f92f6b9b263312464ae7e827fa99a94cf
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-03-25 15:03:00 -0400

    Test: Improved LinkedList and Node tests

--- src/dataStructures/LinkedList/LinkedList.test.js
+++ src/dataStructures/LinkedList/LinkedList.test.js
@@ -2,7 +2,7 @@ import LinkedList from './LinkedList';
 import Node from '../Node/Node';
 
 describe('LinkedList Class tests', () => {
-  it(`Linked LinkedList initializes, adds/removes/gets First, 
+  it(`When valid node passed in, Linked LinkedList initializes, adds/removes/gets First, 
   adds/removes/gets Last, and clears correctly`, () => {
     const newNode = new Node(1);
     let newList = new LinkedList(newNode);
@@ -15,6 +15,8 @@ describe('LinkedList Class tests', () => {
       head: { next: { next: null, val: 1 }, val: 3 },
     });
 
+    expect(newList.size()).toEqual(2);
+
     newList.addLast(5);
 
     expect(newList).toEqual({
@@ -25,15 +27,13 @@ describe('LinkedList Class tests', () => {
       },
     });
 
-    const first = newList.getFirst();
-    expect(first).toEqual({
+    expect(newList.getFirst()).toEqual({
       next: { next: { next: null, val: 5 }, val: 1 },
 
       val: 3,
     });
 
-    const last = newList.getLast();
-    expect(last).toEqual({ next: null, val: 5 });
+    expect(newList.getLast()).toEqual({ next: null, val: 5 });
 
     newList.removeLast();
 
@@ -48,5 +48,68 @@ describe('LinkedList Class tests', () => {
     newList.clear();
 
     expect(newList).toEqual({ head: null });
+  });
+
+  it(`When no node passed in, Linked LinkedList initializes, adds/removes/gets First, 
+  adds/removes/gets Last, and clears correctly`, () => {
+    let newList2 = new LinkedList();
+
+    expect(newList2).toEqual({ head: null });
+    expect(newList2.getFirst()).toEqual(null);
+    expect(newList2.getLast()).toEqual(null);
+
+    newList2.removeFirst();
+    newList2.removeLast();
+
+    newList2.addLast(5);
+
+    expect(newList2).toEqual({
+      head: { next: null, val: 5 },
+    });
+
+    newList2.clear();
+
+    expect(newList2).toEqual({ head: null });
+
+    newList2.addFirst(1);
+    newList2.addFirst(3);
+
+    expect(newList2).toEqual({
+      head: { next: { next: null, val: 1 }, val: 3 },
+    });
+
+    expect(newList2.size()).toEqual(2);
+
+    newList2.addLast(5);
+
+    expect(newList2).toEqual({
+      head: {
+        next: { next: { next: null, val: 5 }, val: 1 },
+
+        val: 3,
+      },
+    });
+
+    expect(newList2.getFirst()).toEqual({
+      next: { next: { next: null, val: 5 }, val: 1 },
+
+      val: 3,
+    });
+
+    expect(newList2.getLast()).toEqual({ next: null, val: 5 });
+
+    newList2.removeLast();
+
+    expect(newList2).toEqual({
+      head: { next: { next: null, val: 1 }, val: 3 },
+    });
+
+    newList2.removeFirst();
+
+    expect(newList2).toEqual({ head: { next: null, val: 1 } });
+
+    newList2.clear();
+
+    expect(newList2).toEqual({ head: null });
   });
 });
--- src/dataStructures/Node/Node.test.js
+++ src/dataStructures/Node/Node.test.js
@@ -1,9 +1,15 @@
 import Node from './Node';
 
 describe('Node Class tests', () => {
-  it(`Node initializes correctly`, () => {
+  it(`Node initializes correctly when passed in value`, () => {
     const newNode = new Node(1);
 
     expect(newNode).toEqual({ next: null, val: 1 });
+  });
+
+  it(`Node initializes correctly when no value passed in`, () => {
+    const newNode2 = new Node();
+
+    expect(newNode2).toEqual({ next: null, val: null });
   });
 });
