79c2cfa2f72c788b0b24bf8563c5e9cb0436df2b
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-04-03 18:15:52 -0400

    Style: Add App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
 
 // Bootstrap
 import Container from 'react-bootstrap/Container';
@@ -22,7 +23,7 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      <Container style={{ marginTop: '20px' }}>
+      <Container className="AppContainer">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/sorting" component={Sorting} />
