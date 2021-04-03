39a28e32dd272090fb3d082d5cd70a35b043f3d5
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-04-03 18:22:13 -0400

    Convert to TS: BinaryTreeNode

--- src/dataStructures/BinaryTreeNode/BinaryTreeNode.ts
+++ src/dataStructures/BinaryTreeNode/BinaryTreeNode.ts
@@ -1,4 +1,8 @@
 export default class BinaryTreeNode {
+  val: number | null;
+  right: BinaryTreeNode | null;
+  left: BinaryTreeNode | null;
+
   constructor(val = null, right = null, left = null) {
     this.val = val;
     this.left = left;
