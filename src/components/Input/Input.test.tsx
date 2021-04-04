46413c86c80ed8dc899e77ff7b1f21d760238928
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-04-04 13:31:50 -0400

    Convert to TS: Input

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Input from './Input';
 
 describe('Input tests', () => {
   it('Input correctly displays entered text', () => {
-    render(<Input />);
+    render(<Input placeholder="Enter a new Node value" />);
 
     userEvent.type(screen.getByRole('textbox'), '3');
     expect(screen.getByRole('textbox')).toHaveValue('3');
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Input.module.css';
+
+interface Props {
+  value?: string | number;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+}
+
+const Input = (props: Props) => {
+  return (
+    <input
+      className="Input"
+      value={props.value}
+      onChange={props.onChange}
+      placeholder={props.placeholder}
+    />
+  );
+};
+
+export default Input;
