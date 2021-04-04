ec425ff4c4a4383bc16d5e4afa03c110f7ba8481
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-04-04 13:34:48 -0400

    Convert to TS: MyButton

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Input.module.css';
+import './Input.css';
 
 interface Props {
   value?: string | number;
---
+++ src/components/MyButton/MyButton.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react';
+import './MyButton.css';
+
+interface Props {
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  disabled: boolean;
+  children: string | ReactNode;
+}
+
+const MyButton = (props: Props) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className={props.disabled ? 'MyButtonDisabled' : 'MyButton'}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default MyButton;
