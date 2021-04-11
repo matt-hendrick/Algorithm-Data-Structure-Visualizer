8334d9d0a98893b93b9e6cdbc820c2785c156ec3
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-04-11 15:57:19 -0400

    Convert to TS: utilityFunctions

--- src/utility/utilityFunctions.ts
+++ src/utility/utilityFunctions.ts
@@ -1,34 +1,35 @@
 import { useEffect, useRef } from 'react';
 
-export const useInterval = (callback, delay) => {
-  const savedCallback = useRef();
+export const useInterval = (callback: Function, delay: number) => {
+  const savedCallback = useRef<Function>();
 
   // Remember the latest callback.
   useEffect(() => {
-    savedCallback.current = callback;
+    savedCallback.current = callback as Function;
   }, [callback]);
 
   // Set up the interval.
   useEffect(() => {
     function tick() {
-      savedCallback.current();
+      if (savedCallback.current) savedCallback.current();
+      else return;
     }
     if (delay !== null) {
       let id = setInterval(tick, delay);
       return () => clearInterval(id);
-    }
+    } else return;
   }, [delay]);
 };
 
-export const getRandomInt = (min, max) => {
+export const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-export const swap = (array, from, to) => {
+export const swap = (array: number[], from: number, to: number) => {
   [array[from], array[to]] = [array[to], array[from]];
 };
 
-export const shuffle = (array) => {
+export const shuffle = (array: number[]) => {
   for (let index = 0; index < array.length; index++) {
     const newIndex = Math.floor(Math.random() * array.length);
     swap(array, index, newIndex);
@@ -36,7 +37,7 @@ export const shuffle = (array) => {
   return array;
 };
 
-export const isSorted = (array) => {
+export const isSorted = (array: number[][]) => {
   for (let i = 1; i < array.length; i++) {
     if (array[i - 1][0] > array[i][0]) {
       return false;
